85310f8cb16cf27c4cedca62c87191ce757b1f5d
Softwarica College Lab <softwarica@softwarica.com>
2019-05-21 11:16:57 +0545

    Initial commit

---
+++ app/src/main/java/np/com/softwarica/heroesapi/AddHeroActivity.java
@@ -0,0 +1,83 @@
+package np.com.softwarica.heroesapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import np.com.softwarica.heroesapi.api.HeroesAPI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AddHeroActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText etName,etDesc;
+    private Button btnSave;
+    public static final  String BASE_URL="http://10.0.2.2:3000/";
+    private ImageView imgProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hero);
+        etName=findViewById(R.id.etName);
+        etDesc=findViewById(R.id.etDesc);
+        btnSave=findViewById(R.id.btnSave);
+        imgProfile=findViewById(R.id.imgProfile);
+
+        btnSave.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.btnSave)
+        {
+            Save();
+        }
+
+    }
+    private void Save(){
+        String name=etName.getText().toString();
+        String desc=etDesc.getText().toString();
+
+        Map<String,String> map=new HashMap<>();
+        map.put("name",name);
+        map.put("desc",desc);
+
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        HeroesAPI heroesAPI= retrofit.create(HeroesAPI.class);
+//        Call<Void> heroesCall=heroesAPI.addHero(name,desc);
+        Call<Void>  heroesCall=heroesAPI.addHero(map);
+        heroesCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(AddHeroActivity.this,"Code" + response.code(),Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(AddHeroActivity.this,"Successfully Added",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(AddHeroActivity.this,"Error" + t.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
